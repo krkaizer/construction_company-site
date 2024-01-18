8e737632e656f1040d7edf3cfdc606d7ce4b00fe
Ksenia <139823788+krkaizer@users.noreply.github.com>
2024-01-18 20:20:25 +0100

    materialize nav menu

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() { //когда построено дом-дерево
+    var elems = document.querySelectorAll('.sidenav'); 
+    var instances = M.Sidenav.init(elems); 
+  });
