84e82a537832fa7f8e2bec1fc79d912717522b38
Ksenia <139823788+krkaizer@users.noreply.github.com>
2024-01-18 21:21:23 +0100

    request modal windows materialize

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() { //когда построено дом-дерево
-    var elems = document.querySelectorAll('.sidenav'); 
-    var instances = M.Sidenav.init(elems); 
-  });
+document.addEventListener('DOMContentLoaded', function () { //когда построено дом-дерево
+    //инициализация меню
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instances_sidenav = M.Sidenav.init(sidenav);
+    //инициализация модального окна
+    var modal = document.querySelectorAll('.modal');
+    var instances_modal = M.Modal.init(modal);
+});
